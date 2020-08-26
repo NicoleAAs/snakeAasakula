da23162ca28ca89ff3859fc0fb733b3ff690c6d4
opilane <opilane@HP-CZC6297B9D>
2020-08-26 11:18:04 +0300

    HorizontalLine

---
+++ snakes/HorizontalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snakes
+{
+    class HorizontalLine
+    {
+        List<point> pList;
+
+        public HorizontalLine(int xLeft, int xReight, int y, char sym)
+        {
+            pList = new List<point>();
+            for (int x = xLeft; x <= xReight; x++ )
+            {
+                point p = new point(x, y, sym);
+                pList.Add(p);
+            }
+          
+        }
+        public void Drow()
+        {
+            foreach (point p in pList )
+            {
+                p.Draw();
+            }
+        }
+
+    }
+}
--- snakes/Program.cs
+++ snakes/Program.cs
@@ -13,23 +13,10 @@ namespace snakes
             point p2 = new point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
+            HorizontalLine line = new HorizontalLine(5 , 10 , 8 , '+');
+            line.Drow();
 
-            numList.Add( 0 );
-            numList.Add( 1 );
-            numList.Add( 2 );
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-            numList.RemoveAt(0);
-            List<point> plist = new List<point>();
-            plist.Add(p1);
-            plist.Add(p2);
+            
             Console.ReadLine();
         }
 
