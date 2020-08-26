4b33d3a2e4fa66e9f8b8d4f36c50322a26f13dfd
opilane <opilane@HP-CZC6297B9D>
2020-08-26 10:59:21 +0300

    List

--- snakes/Program.cs
+++ snakes/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace snakes
-using System;
-
-namespace snakee
 {
     class Program
     {
@@ -15,6 +13,23 @@ namespace snakee
             point p2 = new point(4, 5, '#');
             p2.Draw();
 
+            List<int> numList = new List<int>();
+
+            numList.Add( 0 );
+            numList.Add( 1 );
+            numList.Add( 2 );
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            numList.RemoveAt(0);
+            List<point> plist = new List<point>();
+            plist.Add(p1);
+            plist.Add(p2);
             Console.ReadLine();
         }
 
