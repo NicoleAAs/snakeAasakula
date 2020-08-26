a19ec4ddaaf3f61f2677e775cc008f54f7619a40
opilane <opilane@HP-CZC6297B9D>
2020-08-26 11:56:59 +0300

    VerticalLine

--- snakes/Program.cs
+++ snakes/Program.cs
@@ -7,17 +7,35 @@ namespace snakes
     {
         static void Main(string[] args)
         {
-            point p1 = new point(1, 3, '*');
-            p1.Draw();
-
-            point p2 = new point(4, 5, '#');
-            p2.Draw();
-
-            HorizontalLine line = new HorizontalLine(5 , 10 , 8 , '+');
-            line.Drow();
-
+            //Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 27);
             
-            Console.ReadLine();
+            
+
+
+
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine DownLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            DownLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+            point p = new point(4, 5, '*');
+            p.Draw();
+            //point p1 = new point(1, 3, '*');
+            //p1.Draw();
+
+            //point p2 = new point(4, 5, '#');
+            //p2.Draw();
+
+            //HorizontalLine line = new HorizontalLine(5 , 10 , 8 , '+');
+            //line.Drow();
+
+
+            //Console.ReadLine();
         }
 
     }
---
+++ snakes/VerticalLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snakes
+{
+    class VerticalLine
+    {
+        List<point> pList;
+
+        public VerticalLine( int yUp, int yDown, int x, char sym)
+        {
+            pList = new List<point>();
+            for(int y = yUp; y <= yDown; y++)
+            {
+                point p = new point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+        public void Drow()
+        {
+            foreach(point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
