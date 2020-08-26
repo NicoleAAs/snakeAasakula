a9988f24598a7913c74f585d73f85414020d32e3
Opilane TTHK <opilane@tthk.ee>
2020-08-26 10:29:38 +0300

    Add project files.

---
+++ snakes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace snakes
+using System;
+
+namespace snakee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            point p1 = new point(1, 3, '*');
+            p1.Draw();
+
+            point p2 = new point(4, 5, '#');
+            p2.Draw();
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ snakes/point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snakes
+{
+    class point
+    {
+        public int x;
+        public int y;
+        public char sym;
+        public point()
+        {
+        }
+        public point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
